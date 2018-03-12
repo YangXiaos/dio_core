ec33f4c541a09320a2a6eff69b5c0266699fe28e
YangXiaos <178069857@qq.com>
2018-03-12 21:10:25 +0800

    更改安装包描述

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 setup(
     name="dioCore",
     version="0.0.2",
-    description="dio采集系统核心框架",
+    description="dio采集系统核心",
     author="dio_mryang",
     url="https://github.com/YangXiaos/",
     packages=find_packages()

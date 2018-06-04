e450e6a8655a1f978ce956747e2127ee7aa67387
YangXiaos <178069857@qq.com>
2018-06-04 22:32:02 +0800

    添加了json 函数

---
+++ DioCore/Units/JsonUnit.py
@@ -0,0 +1,23 @@
+# @Time         : 18-6-4 下午10:25
+# @Author       : DioMryang
+# @File         : JsonUnit.py
+# @Description  :
+import json
+
+
+def toJson(obj):
+    """
+    python对象 生成json 格式字符串
+    :param obj: 对象
+    :return:
+    """
+    return json.dumps(obj)
+
+
+def formJson(jsonString):
+    """
+    json字符串 生成json 对象
+    :param obj:
+    :return:
+    """
+    return json.loads(jsonString)

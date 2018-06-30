fbb9fee15103250a51d23f833bc250cf1c5ae6f4
YangXiaos <178069857@qq.com>
2018-06-30 15:30:43 +0800

    添加了时间工具类

---
+++ DioCore/Units/TimeUnit.py
@@ -0,0 +1,15 @@
+# @Time         : 18-6-30 下午3:24
+# @Author       : DioMryang
+# @File         : TimeUnit.py
+# @Description  :
+
+import time
+
+
+def sleep(second):
+    """
+    暂停一段时间
+    :param second: 秒数
+    :return:
+    """
+    time.sleep(second)

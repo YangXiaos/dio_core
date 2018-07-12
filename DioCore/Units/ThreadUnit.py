6f616ce3f44435d965fe20db7289cccef3e16cff
YangXiaos <178069857@qq.com>
2018-07-12 22:15:49 +0800

    多线程函数优化

--- DioCore/Units/ThreadUnit.py
+++ DioCore/Units/ThreadUnit.py
@@ -15,16 +15,23 @@ def getCurrentThreadName():
     return threading.current_thread().getName()
 
 
-def multiThreadingRun(fuc, argsList=None, threadNum=5):
+def multiThreadingRun(fuc, argsList=None, threadNum=5, commonArgs=None, commonKwargs=None):
     """
     多线程处理函数
+    :param commonKwargs:
+    :param commonArgs: 共同参数
     :param threadNum:
     :param fuc:
     :param argsList:
     :return:
     """
-    if argsList is None:
+    if commonArgs is not None:
+        argsList = [(commonArgs, None) for _ in range(threadNum)]
+    elif commonKwargs is not None:
+        argsList = [(commonKwargs, None) for _ in range(threadNum)]
+    elif argsList is None:
         argsList = range(threadNum)
+
     pool = threadpool.ThreadPool(threadNum)
     requests = threadpool.makeRequests(fuc, argsList)
     [pool.putRequest(req) for req in requests]
--- DioCore/__init__.py
+++ DioCore/__init__.py
@@ -3,5 +3,5 @@
 # @File         : __init__.py.py
 # @Description  : 工具库
 
-__version__ = '0.1.9'
+__version__ = '0.2.0'
 __author__ = 'dio'
